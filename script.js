bf8d0aae0c4d3c4fa2306cef5e61200d8391ce19
thomas <tom.angotti11@gmail.com>
2022-12-04 10:07:26 -0700

    updated buttons and locations

--- script.js
+++ script.js
@@ -1,4 +1,51 @@
-const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+const career = document.getElementById('career.change');
+const aboutMe = document.getElementById('about.me');
+const workExp = document.getElementById('work.exp');
+const edu = document.getElementById('edu');
+const project = document.getElementById('myProjects');
 
-document.querySelector("body").appendChild(h2);
+career.style.display = "none";
+aboutMe.style.display = "";
+workExp.style.display = "none";
+edu.style.display = "none";
+project.style.display = "none"
+
+function myCareer(){
+    career.style.display = "";
+    aboutMe.style.display = "none";
+    workExp.style.display = "none";
+    edu.style.display = "none";
+    project.style.display = "none";
+};
+
+function myAboutMe(){
+    career.style.display = "none";
+    aboutMe.style.display = "";
+    workExp.style.display = "none";
+    edu.style.display = "none";
+    project.style.display = "none";
+};
+
+function myWorkExp(){
+    career.style.display = "none";
+    aboutMe.style.display = "none";
+    workExp.style.display = "";
+    edu.style.display = "none";
+    project.style.display = "none";
+};
+
+function myEdu(){
+    career.style.display = "none";
+    aboutMe.style.display = "none";
+    workExp.style.display = "none";
+    edu.style.display = "";
+    project.style.display = "none";
+};
+
+function myProjects(){
+    career.style.display = "none";
+    aboutMe.style.display = "none";
+    workExp.style.display = "none";
+    edu.style.display = "none";
+    project.style.display = "";
+};
