951058cbaebf50196e5dd3fc3535eb37f99e95cb
thomas <tom.angotti11@gmail.com>
2023-01-03 17:04:03 -0700

    reworked document loading

---
+++ data.js
@@ -0,0 +1,77 @@
+const jobs = [
+    
+    {
+        company: "Wells Concrete",
+        title: "Design Engineer",
+        year: "Jan 2019 - Nov 2022",
+        responsibilites: `As a Design Engineer my responsibilites were to lead projects 
+        during the design phase and coordinate with both in house and out 
+        of house teams to ensure the project was on schedule for the entire team. 
+        I was also in charge of the full design of the project from lateral analysis 
+        to bearing conditions to structural reinforcement.`
+    },
+    {
+        company: "Rocky Mountain Prestress",
+        title: "Engineer-in-training",
+        year: "Feb 2018 - Jan 2019",
+        responsibilites: `As an Engineer in training I would assit the Project Engineer
+        with member design and connection design. I would resolve any 
+        NCR's (Non Conformance Reports) that would come from the Plant 
+        or the field. I would also check piece drawing to ensure they would match the engineer's design`
+    },
+    {
+        company: "Rocky Mountain Prestress",
+        title: "Field Engineer",
+        year: "Aug 2017 - Feb 2018",
+        responsibilites: `I was at the job site Every day checking construction details ensure the building was following Engineer's design.
+        As well as helping the working by answering questions about details or procedures needing to be done. 
+        I would also coordinate with the Engineer and contractors to resolve any issues or problems.`
+    },
+    {
+        company: "Rocky Mountain Prestress",
+        title: "Quality Control Lead",
+        year: "Jan 2017 - Aug 2017",
+        responsibilites: `My Responsibilities included all of the QC Inspector and I was also
+        tasked with coordinating the tasks of all the QC inspectors and their Responsibilities.
+        I would coordinate with other team leads and Supervisors to resolve any plant issues.`
+    },
+    {
+        company: "Rocky Mountain Prestress",
+        title: "Quality Control Inspector",
+        year: "Aug 2016 - Jan 2017",
+        responsibilites: `I was tasked with checking concrete piece before and after production to ensure the 
+        engineer's design and any PCI or ACI requirements were met. Any thing from the geotry 
+        to welding details to the finish of the piece. I would also coordinate 
+        any NCR's (Non Conformance Reports) with the engineer or Project Manager to get the 
+        best solution for the issue.`
+    },
+]
+
+const education = [
+    {
+        school: "Flatiron School",
+        year: "Dec 2022 - March 2023 (estimated)",
+        description:"Software Engineering Course. Coding languages are HTML, CSS, JavaScript, React, Ruby and Rails."
+    },
+    {
+        school: "West Virginia University",
+        year: "Aug 2011 - April 2016",
+        description:"Bachelors of Science in Civil Engineering with a focus in Structual design. A minor is Business."
+    }
+]
+
+const projects = {
+    software: [
+        {
+
+        }
+    ],
+    structural: [
+        {
+            name: "",
+            building: "",
+            location: "",
+            image: "",
+        }
+    ]
+}
--- script.js
+++ script.js
@@ -1,86 +1,127 @@
-/*NAV BUTTON OPTIONS*/
+/////////////////////////////////////////
+///////// Nav button ////////////////////
+/////////////////////////////////////////
 
-const career = document.getElementById('career.change');
+const structProject = document.getElementById('civil');
 const aboutMe = document.getElementById('about.me');
-const workExp = document.getElementById('work.exp');
+const workExp = document.getElementById('work-exp');
 const edu = document.getElementById('edu');
-const project = document.getElementById('myProjects');
+const softProject = document.getElementById('software');
 
-career.style.display = "none";
+const btnAboutMe = document.querySelector('#about-me-button');
+const btnWork = document.querySelector('#work-exp-button');
+const btnEdu = document.querySelector('#edu-button');
+const btnSoftware = document.querySelector('#software-button');
+const btnStructural = document.querySelector('#structural-button');
+
+btnAboutMe.addEventListener('click', ()=> {
+    myAboutMe();
+});
+btnWork.addEventListener('click', () => {
+    myWorkExp()
+});
+btnEdu.addEventListener('click', () => {
+    myEdu()
+});
+btnSoftware.addEventListener('click', () => {
+    codingProjects()
+});
+btnStructural.addEventListener('click', () => {
+    structProjects()
+});
+
+
+structProject.style.display = "none";
 aboutMe.style.display = "";
 workExp.style.display = "none";
 edu.style.display = "none";
-project.style.display = "none"
+softProject.style.display = "none"
 
-function myCareer(){
-    career.style.display = "";
+function structProjects(){
+    structProject.style.display = "";
     aboutMe.style.display = "none";
     workExp.style.display = "none";
     edu.style.display = "none";
-    project.style.display = "none";
+    softProject.style.display = "none";
 };
 
 function myAboutMe(){
-    career.style.display = "none";
+    structProject.style.display = "none";
     aboutMe.style.display = "";
     workExp.style.display = "none";
     edu.style.display = "none";
-    project.style.display = "none";
+    softProject.style.display = "none";
 };
 
 function myWorkExp(){
-    career.style.display = "none";
+    structProject.style.display = "none";
     aboutMe.style.display = "none";
     workExp.style.display = "";
     edu.style.display = "none";
-    project.style.display = "none";
+    softProject.style.display = "none";
 };
 
 function myEdu(){
-    career.style.display = "none";
+    structProject.style.display = "none";
     aboutMe.style.display = "none";
     workExp.style.display = "none";
     edu.style.display = "";
-    project.style.display = "none";
+    softProject.style.display = "none";
 };
 
-function myProjects(){
-    career.style.display = "none";
+function codingProjects(){
+    structProject.style.display = "none";
     aboutMe.style.display = "none";
     workExp.style.display = "none";
     edu.style.display = "none";
-    project.style.display = "";
+    softProject.style.display = "";
 };
 
 
-/*NAV BUTTON OPTIONS*/
+////////////////////////////////////////////
+////////// Render Job Cards ////////////////
+////////////////////////////////////////////
+jobs.forEach(renderJobs);
 
+function renderJobs(job){
+    let title = document.createElement('h3');
+    let company = document.createElement('h4');
+    let year = document.createElement('h4');
+    let responsibilities = document.createElement('p');
+    let innerDiv = document.createElement('div')
+    innerDiv.className = 'jobs';
 
-/*Construction worker walking*/
+    title.textContent = job.title;
+    company.textContent = job.company;
+    year.textContent = job.year;
+    responsibilities.textContent = job.responsibilites;
 
-let nIntervID;
+    let div = document.querySelector('#work-exp')
 
-function changeImage() {
-    if(!nIntervID) {
-        nIntervID = setInterval(flashImage, 1000);
-    }
+    div.append(innerDiv);
+    innerDiv.append(title, company, year, responsibilities);
+
 }
 
-function flashImage() {
-    const oColor = document.getElementById("test1");
+////////////////////////////////////////////
+////////// Render School Cards /////////////
+////////////////////////////////////////////
+
+education.forEach(renderSchool)
+
+function renderSchool(education) {
+    let school = document.createElement('h3');
+    let year = document.createElement('h4');
+    let description = document.createElement('p');
+    let innerDiv = document.createElement('div')
+    innerDiv.className = 'school';
+
+    school.textContent = education.school;
+    year.textContent = education.year;
+    description.textContent = education.description;
+
     
-    if(oColor.style.color === "red"){
-        oColor.style.color = "blue";
-    } else if(oColor.style.color === "blue"){
-        oColor.style.color = "green";
-    } else {
-        oColor.style.color = "red";
-    }
+    edu.append(innerDiv);
+    innerDiv.append(school, year, description);
+    
 }
-
-document.getElementById("career.change.button").addEventListener("click", changeImage);
-
-
-
-
-/*Construction worker walking*/
